f4a5aae1972987d9bc718c3e76e63e100ba343f7
payiz-asj <1778029840@qq.com>
2020-12-23 14:23:49 +0800

    封装SqlHelper

--- NJU足球赛程管理系统/NJU足球赛程管理系统/MainWindow.xaml.cs
+++ NJU足球赛程管理系统/NJU足球赛程管理系统/MainWindow.xaml.cs
@@ -13,8 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data.SqlClient;
-
-
+using System.Configuration;
 
 namespace NJU足球赛程管理系统
 {
@@ -30,15 +29,19 @@ namespace NJU足球赛程管理系统
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //这里是测试连接按钮点击事件
-            //编写数据库连接串
-            //string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;User ID=payiz;Password=payiz"; //数据库用户名和密码登录
-            string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;Integrated Security=SSPI"; //windows本地身份登录
+            //这里是测试连接按钮点击事件，目的是检测数据库连接是否正常
 
-            //创建SqlConnection的实例
+            //1. 编写数据库连接串
+            //string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;User ID=payiz;Password=payiz"; //数据库用户名和密码登录
+            //string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;Integrated Security=SSPI"; //windows本地身份登录
+
+            //这里是更高级的用法，将数据库连接串放到配置文件App.config中，这样更加方便
+            string connStr = ConfigurationManager.ConnectionStrings["dbConnStr"].ConnectionString;
+
+            //2. 检测数据库连接是否正常
             try
             {
-                //using 关键字是用来自动释放资源
+                //创建SqlConnection的实例，using 关键字是用来自动释放资源
                 using (SqlConnection conn = new SqlConnection(connStr))
                 {
                     //打开数据库连接
@@ -50,7 +53,6 @@ namespace NJU足球赛程管理系统
             {
                 MessageBox.Show("数据库连接失败！\n程序将无法提供正常服务，请谅解！\n\n您也可以按以下信息排查错误：\n" + ex.Message, "连接状态通报", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
---
+++ NJU足球赛程管理系统/NJU足球赛程管理系统/SqlHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NJU足球赛程管理系统
+{
+    class SqlHelper
+    {
+        //这里是数据库操作助手类，封装了数据库的链接、打开，执行命令等操作
+
+        //1. 编写数据库连接串
+        //string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;User ID=payiz;Password=payiz"; //数据库用户名和密码登录
+        //string connStr = "Data source=LAPTOP-AGT30UAM\\SQLEXPRESS;Initial Catalog=NJU_FOOTBALL;Integrated Security=SSPI"; //windows本地身份登录
+
+        //这里是更高级的用法，将数据库连接串放到配置文件App.config中，这样更加方便
+        private static string connStr = ConfigurationManager.ConnectionStrings["dbConnStr"].ConnectionString;
+
+        //2. 数据库操作
+        public static int ExcuteNonQuery(string sql)
+        {
+            //创建SqlConnection的实例，using 关键字是用来自动释放资源
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                //打开数据库连接
+                conn.Open();
+                //执行数据库命令
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = sql;
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        public static object ExecuteScalar(string sql)
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = sql;
+                    return cmd.ExecuteScalar();
+                }
+            }
+        }
+
+
+
+    }
+}
